6695aba3b378a0dc4a52a453510d2ddb8f6e6547
Temitope Ayobami <you@temtopeayobami@gmail.com>
2023-06-30 21:12:54 +0100

    Uploading to my initial commit

--- project_folder/settings.py
+++ project_folder/settings.py
@@ -1,3 +1,5 @@
+
+
 """
 Django settings for project_folder project.
 
@@ -10,6 +12,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

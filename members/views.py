2291b533e1b2f3c5b8035b1130faa0885346e2b1
“Temitope <temtopeayobami@gmail.com>
2023-08-02 12:10:33 +0100

    done

--- members/views.py
+++ members/views.py
@@ -7,8 +7,8 @@ from django import forms
 
 def login_user(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)

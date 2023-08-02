b50f302f3e64760e23c5a1463cf9c108b7f5f1ec
“Temitope <temtopeayobami@gmail.com>
2023-08-02 12:04:42 +0100

    done

--- project_folder/settings.py
+++ project_folder/settings.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-
+'''
 import environ
 
 env = environ.Env()
@@ -23,7 +23,7 @@ env = environ.Env()
 
 environ.Env.read_env()
 
-
+'''
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'project_folder.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -99,7 +99,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

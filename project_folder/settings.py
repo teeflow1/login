ec85cd6123a2c1a3f2565b5437451f1a0fd9d2ad
“Temitope <temtopeayobami@gmail.com>
2023-08-02 12:26:05 +0100

    Done

--- project_folder/settings.py
+++ project_folder/settings.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-'''
+
 import environ
 
 env = environ.Env()
@@ -23,7 +23,11 @@ env = environ.Env()
 
 environ.Env.read_env()
 
-'''
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
